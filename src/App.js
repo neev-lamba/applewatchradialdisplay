da10bdd2ab0c153c5cd35d0d92a48a991ebe7e39
Neev Singh Lamba <52173761+neev-lamba@users.noreply.github.com>
2023-07-13 15:04:31 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RadialChart from './components/RadialChart';
+
+function App() {
+  return (
+    <div className="App">
+      <RadialChart></RadialChart>
+    </div>
+  );
+}
+
+export default App;

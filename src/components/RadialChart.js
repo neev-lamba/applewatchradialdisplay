a084e990128a92d95359d515e9d3ed9773bb5fe5
Neev Singh Lamba <52173761+neev-lamba@users.noreply.github.com>
2023-07-13 15:09:08 -0400

    Add files via upload

--- src/components/RadialChart.js
+++ src/components/RadialChart.js
@@ -5,6 +5,7 @@ class RadialChart extends React.Component {
   componentDidMount() {
     this.buildChart();
   }
+  
   buildChart() {
     const gap = 2;
     const dataset = [
@@ -16,27 +17,27 @@ class RadialChart extends React.Component {
     const width = 500;
     const height = 500;
     const τ = 2 * Math.PI;
-  
+
     const arc = d3.arc()
       .startAngle(0)
       .endAngle((d) => (d.percentage / 100) * τ)
       .innerRadius((d) => 140 - d.index * (40 + gap))
       .outerRadius((d) => 180 - d.index * (40 + gap))
       .cornerRadius(20);
-  
+
     const background = d3.arc()
       .startAngle(0)
       .endAngle(τ)
       .innerRadius((d, i) => 140 - d.index * (40 + gap))
       .outerRadius((d, i) => 180 - d.index * (40 + gap));
-  
+
     const svg = d3.select("#radial-chart")
       .append("svg")
       .attr("width", width)
       .attr("height", height)
       .append("g")
       .attr("transform", `translate(${width / 2},${height / 2})`);
-  
+
     const gradient = svg.append("defs")
       .append("linearGradient")
       .attr("id", "gradient")
@@ -45,59 +46,59 @@ class RadialChart extends React.Component {
       .attr("x2", "50%")
       .attr("y2", "0%")
       .attr("spreadMethod", "pad");
-  
+
     gradient.append("stop")
       .attr("offset", "0%")
       .attr("stop-color", "#fe08b5")
       .attr("stop-opacity", 1);
-  
+
     gradient.append("stop")
       .attr("offset", "100%")
       .attr("stop-color", "#ff1410")
       .attr("stop-opacity", 1);
-  
+
     const defs = svg.append("defs");
-  
+
     const filter = defs.append("filter")
       .attr("id", "dropshadow");
-  
+
     filter.append("feGaussianBlur")
       .attr("in", "SourceAlpha")
       .attr("stdDeviation", 4)
       .attr("result", "blur");
-  
+
     filter.append("feOffset")
       .attr("in", "blur")
       .attr("dx", 1)
       .attr("dy", 1)
       .attr("result", "offsetBlur");
-  
+
     const feMerge = filter.append("feMerge");
-  
+
     feMerge.append("feMergeNode")
       .attr("in", "offsetBlur");
     feMerge.append("feMergeNode")
       .attr("in", "SourceGraphic");
-  
+
     const field = svg.selectAll("g")
       .data(dataset)
       .enter()
       .append("g");
-  
+
     field.append("path")
       .attr("class", "progress")
       .attr("filter", "url(#dropshadow)")
       .style("fill", (d) => (d.index === 0 ? "url(#gradient)" : colors[d.index]))
       .attr("d", arc);
-  
+
     field.append("path")
       .attr("class", "bg")
       .style("fill", (d) => colors[d.index])
       .style("opacity", 0.2)
       .attr("d", background);
-  
+
     const singleArcView = true; // Set this to true to display current value on only one radial bar
-  
+
     if (singleArcView) {
       const completedText = field.append("text")
         .attr('class', 'completed')
@@ -107,7 +108,7 @@ class RadialChart extends React.Component {
         .style("alignment-baseline", "middle")
         .style("font-size", "28px")
         .style("fill", "black");
-  
+
       const goalText = field.append("text")
         .attr('class', 'goal')
         .text("OF 600 CALS")
@@ -117,7 +118,7 @@ class RadialChart extends React.Component {
         .style("alignment-baseline", "middle")
         .style("font-size", "12px")
         .style("fill", "black");
-  
+
       field.each(function (d, i) {
         if (i === 0) {
           const percentageValue = Math.round((d.percentage / 100) * 600);
@@ -138,31 +139,31 @@ class RadialChart extends React.Component {
         .style("font-size", "20px")
         .style("fill", "black");
     }
-  
+
     update();
-  
+
     function update() {
       field.each(function (d) {
         this._value = d.percentage;
       });
-  
+
       field.select("path.progress")
         .transition()
         .duration(1750)
         .delay((d, i) => i * 200)
         .ease(d3.easeElastic)
         .attrTween("d", arcTween);
-  
+
       field.select("text.completed")
         .text((d, i) => {
           if (i === 0) {
             return Math.round((d.percentage / 100) * 600);
           }
         });
-  
+
       setTimeout(update, 2000);
     }
-  
+
     function arcTween(d) {
       const i = d3.interpolateNumber(d.previousValue || 0, d.percentage);
       return function (t) {
@@ -171,9 +172,7 @@ class RadialChart extends React.Component {
       };
     }
   }
-  
-  
-  
+
   render() {
     return <div id="radial-chart"></div>;
   }
